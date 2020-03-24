f918eb17897a3db915344b227ad28d091ad5020f
Leah Pronk <2109208@talnet.nl git config --global user.name Leah>
2020-03-24 12:10:06 +0100

    Uitwerking van de exercise

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-document.querySelector('.titel').innerText = document.title;
+let h1 = document.querySelector("h1");
+let title = document.querySelector("title");
+title.innerText = h1.innerText;
+
+h1.classList;
+h1.classList.add('h1');
